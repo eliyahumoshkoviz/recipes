f3d8298e6285f6bc60a3f690b898454cc35f76f5
eliyahumoshkoviz <054epm@gmail.com>
2024-05-21 17:23:44 +0300

    label

--- app/page.js
+++ app/page.js
@@ -5,7 +5,7 @@ import Label from "@/components/Label";
 export default function Home() {
   return (
     <main className={styles.main}>
-      {/* <Label data={'מנות עיקריות'} color={'green'} /> */}
+      <Label data={'מנות עיקריות'} color={'green'} />
       <div className={styles.search}>
         <SearchBar />
       </div>
