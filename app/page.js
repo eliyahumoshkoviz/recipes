d484096a1a31e1803e7406c3f146a9d34ea8ced9
AviadDerli <aviad@workin.co.il>
2024-05-21 12:32:56 +0300

    fix code element

--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ export default function Home() {
       <div className={styles.description}>
         <p>
           Get started by  editing&nbsp;
-          <code clasrevert-1-shaulsName={styles.code}>app/page.js</code>
+          <code className={styles.code}>app/page.js</code>
         </p>
         <div>
           <a
