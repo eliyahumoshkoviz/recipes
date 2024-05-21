0107f4809d42bca378a4d7b76a37f653abf19b39
meir vitkin <vitkin4580@email.com>
2024-05-21 22:44:01 +0300

    add handleChange function

--- components/SearchBar/index.jsx
+++ components/SearchBar/index.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+"use client"
 import styles from './style.module.scss';
+import { useRouter } from 'next/router';
 
 export const SearchBar = () => {
+
+    // const router = useRouter()
+   
+    const handleSearch = e => {
+       if (e.target.value) {
+            console.log(e.target.value);
+        //   router.push(`/airbnb/result?search=${e.target.value}`)
+       } else {
+        //    router.push('/')
+       }
+    }
     return (
         <form className={styles.container} action="">
             <input
@@ -11,6 +23,7 @@ export const SearchBar = () => {
                 name="search"
                 required
                 autoComplete="off"
+                onChange={handleSearch}
             />
             <img
                 src="https://www.10dakot.co.il/wp-content/themes/10dakot/assets/img/search.png"
