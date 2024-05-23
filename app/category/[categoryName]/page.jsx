e88fa2a1186bcf6e885e563a5bc70acf2f9c85e2
unknown <a0548428751@gmail.com>
2024-05-23 14:12:19 +0300

    color label

--- app/category/[categoryName]/page.jsx
+++ app/category/[categoryName]/page.jsx
@@ -5,10 +5,11 @@ import { createCategorysService } from "@/server/DB/category.service";
 export default async function Category() {
   await connectToMongo();
   const recipes = []
-  // const x = {
-  //   name: "cakes",
-  //   image: "https://img.lovepik.com/bg/20231213/an-angry-cat-looking-in-the-dark_2452200_wh1200.png",
-  // };
+  const x = {
+    name: "cakes",
+    image: "https://img.lovepik.com/bg/20231213/an-angry-cat-looking-in-the-dark_2452200_wh1200.png",
+    colorLabel: "#c24a7d"
+  };
 
   // const craate = await createCategorysService(x);
   // console.log({craate});
--- components/Instructions/index.jsx
+++ components/Instructions/index.jsx
@@ -1,16 +1,51 @@
-import styles from './style.module.css'
+import styles from './style.module.scss'
 
 export default function Instructions() {
+    const ingredients = ["בשר", "מלח"];
     return (
         <div className={styles.container}>
 
             <div className={styles.header}>
-                <div className={styles.title}> זמן הכנה</div>
-                <div className={styles.title}> זמן בישול/אפיה</div>
-                <div className={styles.title}>  מספר מנות</div>
-                <div className={styles.type}>  מספר מנות</div>
+                <div className={styles.title}>
+                    <h4>
+                        זמן הכנה
+                    </h4>
+                    <div className={styles.content}>
+                        10 דקות
+                    </div>
+                </div>
+                <div className={styles.title}>
+                    <h4>
+                        זמן בישול/אפיה
+                    </h4>
+                    <div className={styles.content}>
+                        30 דקות
+                    </div>
+                </div>
+                <div className={styles.title}>
+                    <h4>
+                        מספר מנות
+                    </h4>
+                    <div className={styles.content}>
+                        25
+                    </div>
+                </div>
+                <div className={styles.type}>
+                    <h4>
+                        סוג
+                    </h4>
+                    <div className={styles.content}>
+                        בשרי
+                    </div>
+                </div>
+
             </div>
             <h1> רכיבים</h1>
+            {
+                <ul>
+            {ingredients.map(item=><li>{item}</li>)}
+                </ul>
+            }
         </div>
     )
 }
