a3c841980edf3172cf86e13d006cbc7e0ee80557
meir vitkin <vitkin4580@email.com>
2024-05-28 15:41:48 +0300

    aded a fake data

--- components/SearchBar/index.jsx
+++ components/SearchBar/index.jsx
@@ -2,33 +2,37 @@
 import { useRouter } from 'next/navigation';
 import styles from './style.module.scss';
 
-export const SearchBar = () => {
+export const SearchBar = ({ val }) => {
 
     const router = useRouter()
-   
+
     const handleSearch = e => {
-       if (e.target.value) {
+        if (e.target.value) {
             console.log(e.target.value);
-          router.push(`/category/extras?search=${e.target.value}`)
-       } else {
-           router.push('/')
-       }
+            router.push(`/result?search=${e.target.value}`)
+        } else {
+            router.push('/')
+        }
     }
     return (
-        <div className={styles.container} action="">
-            <input
-                className={styles.formInput}
-                type="text"
-                placeholder="חפש מתכונים"
-                name="search"
-                autoComplete="off"
-                onChange={handleSearch}
-            />
-            <img
-                src="https://www.10dakot.co.il/wp-content/themes/10dakot/assets/img/search.png"
-                alt="Search"
-                className={styles.img}
-            />
+        <div className={styles.search}>
+            <div className={styles.container} action="">
+                <input
+                    className={styles.formInput}
+                    type="text"
+                    placeholder="חפש מתכונים"
+                    name="search"
+                    value={val || ''}
+                    autoFocus
+                    autoComplete="off"
+                    onChange={handleSearch}
+                />
+                <img
+                    src="https://www.10dakot.co.il/wp-content/themes/10dakot/assets/img/search.png"
+                    alt="Search"
+                    className={styles.img}
+                />
+            </div>
         </div>
     );
 };
