bfb9978af3ae1f2a417fd2cca620ba151f0c0e77
meir vitkin <vitkin4580@email.com>
2024-05-28 21:41:01 +0300

    fixed the SearchBar component and added var.scss file

--- app/result/page.jsx
+++ app/result/page.jsx
@@ -1,24 +1,24 @@
+import Photo from '@/components/Photo'
+import styles from './style.module.scss'
 import { SearchBar } from '@/components/SearchBar'
 import { connectToMongo } from '@/server/DL/connectToMongo'
-import { readCategory } from '@/server/DL/controllers/category.controller'
 import { readRecipes } from '@/server/DL/controllers/recipe.controller'
-import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 export default async function Page({ searchParams: { search } }) {
    await connectToMongo()
    const result = await readRecipes({
-      $or: [{ title: { $regex: search, $options: 'i' } }, { description: { $regex: search, $options: 'i' } }]
+      $or: [{ title: { $regex: search, $options: 'i' } }, { typeFood: { $regex: search, $options: 'i' } }]
    })
 
    return (
       <div>
          <SearchBar val={search}/>
-         <section className='grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-4'>
+         <section className={styles.photoContainer}>
             {result?.map((recipe) => (
-               <Link className='flex flex-col' key={recipe._id} href={`/recipe/${recipe._id}`} >
-                  <Image src={recipe.image} alt={recipe.title} width={600} height={400} />
+               <Link  key={recipe._id} href={`/recipe/${recipe._id}`} >
+                  <Photo data={recipe} typeObj={'recipe'} />
                   {recipe.title}
                </Link>
             ))}
