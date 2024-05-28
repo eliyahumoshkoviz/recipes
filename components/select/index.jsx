e62d117af3f34ad61d826dc0992a104f3ecaa898
unknown <a0548428751@gmail.com>
2024-05-28 17:09:24 +0300

    eli

--- components/select/index.jsx
+++ components/select/index.jsx
@@ -1,29 +1,13 @@
 "use client"
 
-import styles from './style.module.scss'
-import { useRouter } from "next/navigation"
-
-
-export default async function Select({ category }) {
-  const router = useRouter()
+export default function Select({ category }) {
    
-  const handleSearch = e => {
-    console.log(e.target.value);
-    console.log('dfj');
-    //  if (e.target.value) {
-    //   console.log(e.target.value);
-        
-    //  } else {
-    //     router.push('/airbnb')
-    //  }
-  }
   return (
     <div>
-      <label className={styles.label} htmlFor="selectOption">בחר קטגוריה</label>
-      <select className={styles.select} id="selectOption">
+      <select id="selectOption">
         <option>בחר קטגוריה</option>
         {category?.map((categoryItem, index) => (
-            <option className={styles.option} onClick={handleSearch} key={index} value={categoryItem.title}>
+            <option key={index} value={categoryItem.title}>
               {categoryItem.title}
             </option>
         ))}
