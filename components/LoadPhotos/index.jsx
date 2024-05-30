b96c388acd62b07122552f331997f2ba08ba1354
unknown <s0527142427@gmail.com>
2024-05-30 15:03:17 +0300

    loading component

--- components/LoadPhotos/index.jsx
+++ components/LoadPhotos/index.jsx
@@ -4,7 +4,7 @@ import styles from './styles.module.scss'
 const LoadPhoto = () => {
   return (
     <section className={styles.load} >
-    {Array(3).fill('k').map((j, i) => <div className={styles.photoLoad} key={i} ></div>)}
+    {Array(6).fill('k').map((j, i) => <div className={styles.photoLoad} key={i} ></div>)}
  </section>
   )
 }
