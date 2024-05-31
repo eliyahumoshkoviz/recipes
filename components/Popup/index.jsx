5253be62df7b3aeb410867935ae4d188939bb9d9
meir vitkin <vitkin4580@email.com>
2024-05-31 10:02:25 +0300

    fixed the popup css

--- components/Popup/index.jsx
+++ components/Popup/index.jsx
@@ -1,34 +1,34 @@
 import { useEffect, useState } from 'react'
 import Styles from './style.module.scss'
 
-export const Popup = ({ recipeName }) => {
-    const [recipe, setRecipe] = useState({})
+export const Popup = ({recipeName,setPopup}) => {
+    const[recipe,setRecipe]= useState({})
 
 
     useEffect(() => {
-        fetch(`/api/recipe/${recipeName}`, { cache: 'no-store' }).then(res => {
-            res.json().then(data => console.log(data))
-        })
-
-    }, [])
+        fetch(`/api/recipe/${recipeName}`, { cache: 'no-store' })
+            .then(res => res.json())
+            .then(data => { setRecipe(data)})
+            .catch(error => console.error('Fetch error:', error));
+    }, [recipeName]);
 
     const {
-        title,
-        description,
-        image,
-        ingredients,
-        preparationTime,
-        CookingTime,
-        servings,
-        typeFood,
-        instructions,
-        category
+      title,
+      description,
+      image,
+      ingredients,
+      preparationTime,
+      CookingTime,
+      servings,
+      typeFood,
+      instructions,
+      category
     } = recipe;
 
     return (
-        <div className={Styles.Container} >
+        <div className={Styles.Container} onClick={()=>setPopup(false)} >
             <div className={Styles.popupContent}>
-                {description}
+                {title}
             </div>
         </div>
     )
