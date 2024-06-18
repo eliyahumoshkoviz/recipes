95c59738c7658c64a8c5eba042e90ac6f69e9dde
eli <054apm@gmail.com>
2024-06-18 10:27:42 +0300

    add lean at readRecipes

--- server/DL/controllers/recipe.controller.js
+++ server/DL/controllers/recipe.controller.js
@@ -1,6 +1,6 @@
 import { RecipeModel } from "../models/recipe.model"
 
-export const readRecipes = () => RecipeModel.find()
+export const readRecipes = () => RecipeModel.find().lean()
 export const readRecipeById = async (id, populate = false, lean = true) => {
    const recipe = populate ? RecipeModel.findById(id).populate('category') : RecipeModel.findById(id);
    if(lean) recipe.lean();
