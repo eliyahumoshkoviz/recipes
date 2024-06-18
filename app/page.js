3ae414970e1e0d7fce7b6207519decf8feadb430
eli <054apm@gmail.com>
2024-06-18 17:17:12 +0300

    add option to del cat not fix

--- app/page.js
+++ app/page.js
@@ -4,19 +4,27 @@ import Photo from "@/components/Photo";
 import { connectToMongo } from "@/server/DL/connectToMongo";
 import { readCategorysService } from "@/server/DB/category.service";
 import Navlink from "@/components/NavLink";
+import DeleteCategory from "@/components/DeleteCategory";
 
 export default async function Home() {
   await connectToMongo();
-  const category = await readCategorysService()
+  const categories = await readCategorysService();
 
   return (
     <main className={styles.main}>
       <SearchBar />
       <div className={styles.photoContainer}>
-        {category?.map((category, index) => <Navlink key={index} href={`/category/${category.title}`}><Photo data={category} typeObj={"category"} /></Navlink>)}
+        {categories?.map((category, index) => (
+          <div key={index} className={styles.categoryItem}>
+            <Navlink href={`/category/${category.title}`}>
+              <Photo data={category} typeObj={"category"} />
+            </Navlink>
+            {category?.recipes.length === 0 && <div className={styles.btn}>
+                <DeleteCategory categoryId={category._id} />
+            </div>}
+          </div>
+        ))}
       </div>
     </main>
-
-
   );
 }
