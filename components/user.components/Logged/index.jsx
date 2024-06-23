47dfe869b4d57e9ac7c7bdd5905c39c033abd3fe
eliyahumoshkoviz <054epm@gmail.com>
2024-06-23 15:38:08 +0300

    [added from register]

--- components/user.components/Logged/index.jsx
+++ components/user.components/Logged/index.jsx
@@ -4,14 +4,14 @@ import Image from 'next/image'
 
 export default function Logged({ user = { name: 'מאיר', avatar: 'http://res.cloudinary.com/die7h5tgc/image/upload/v1719124786/recipeImage/jgecw5jbkn8ecel9nryu.jpg' } }) {
   return (
-    
-      <div className={styles.login} onClick={()=>{}}>
-        <span className={styles.userAvatar}>
-          <Image src={user.avatar} layout="fill" sizes='100%' alt={"user avatar"} />
-        </span>
-        {`שלום ${user.name}`}
-      </div>
-    
+
+    <div className={styles.login} onClick={() => { }}>
+      <span className={styles.userAvatar}>
+        <Image src={user.avatar} fill sizes='100%' alt={"user avatar"} />
+      </span>
+      {`שלום ${user.name}`}
+    </div>
+
 
   )
 }
