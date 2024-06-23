da53477abb55e058912cc37c13354de3da79ab15
meir vitkin <vitkin4580@email.com>
2024-06-23 12:05:36 +0300

    [ fix loader component and added a register nav in the header]

--- components/Header/index.jsx
+++ components/Header/index.jsx
@@ -1,17 +1,21 @@
 import Navlink from '../NavLink'
 import styles from './style.module.scss'
 import Image from 'next/image'
+import { FaUser } from "react-icons/fa";
 
 export default function Header() {
   return (
     <>
       <div className={styles.container}>
         <nav>
+        <p className={styles.login}> 
+          <span className={styles.userAvatar}>
+            <FaUser/> </span>התחברות/הרשמה</p>
           <Navlink href={'/'}>
             <h4>דף הבית</h4>
           </Navlink>
           <div className={styles.dropdown}>
-            <h4 className={styles.add}>הוסף מתכון/קטגוריה</h4>
+            <h4 className={styles.add}>הוסף מתכון/קטגוריה </h4>
             <div className={styles.menu}>
               <Navlink href={'/createRecipe'}>
                 <h5>הוסף מתכון</h5>
