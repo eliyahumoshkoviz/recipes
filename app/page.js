fde408357023523f0d5209c86dd2df26449baca5
eli <054apm@gmail.com>
2024-06-23 16:12:53 +0300

    add Register or connect selector

--- app/page.js
+++ app/page.js
@@ -5,26 +5,28 @@ import { connectToMongo } from "@/server/DL/connectToMongo";
 import { readCategorysService } from "@/server/DB/category.service";
 import Navlink from "@/components/NavLink";
 import DeleteCategory from "@/components/DeleteCategory";
+import Connect from "@/components/user.components/Connection";
 
 export default async function Home() {
   await connectToMongo();
   const categories = await readCategorysService();
 
   return (
-    <main className={styles.main}>
-      <SearchBar />
-      <div className={styles.photoContainer}>
-        {categories?.map((category, index) => (
-          <div key={index} className={styles.categoryItem}>
-            <Navlink href={`/category/${category.title}`}>
-              <Photo data={category} typeObj={"category"} />
-            </Navlink>
-            {category?.recipes.length === 0 && <div className={styles.btn}>
-                <DeleteCategory categoryId={category._id} />
-            </div>}
-          </div>
-        ))}
-      </div>
-    </main>
+    // <main className={styles.main}>
+    //   <SearchBar />
+    //   <div className={styles.photoContainer}>
+    //     {categories?.map((category, index) => (
+    //       <div key={index} className={styles.categoryItem}>
+    //         <Navlink href={`/category/${category.title}`}>
+    //           <Photo data={category} typeObj={"category"} />
+    //         </Navlink>
+    //         {category?.recipes.length === 0 && <div className={styles.btn}>
+    //             <DeleteCategory categoryId={category._id} />
+    //         </div>}
+    //       </div>
+    //     ))}
+    //   </div>
+    // </main>
+    <Connect/>
   );
 }
