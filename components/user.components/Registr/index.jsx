221af274fc294de508935330784fbadaa36de6e9
eliyahumoshkoviz <054epm@gmail.com>
2024-06-23 14:12:28 +0300

    add .

--- components/user.components/Registr/index.jsx
+++ components/user.components/Registr/index.jsx
@@ -1,8 +1,62 @@
-import React from 'react'
-import styles from './style.module.scss'
+'use client'
+
+import { useState } from "react";
+import styles from './style.module.scss';
+import { CiUser } from "react-icons/ci";
+import { HiOutlineMail } from "react-icons/hi";
+import { TbPassword } from "react-icons/tb";
+import { FiEye, FiEyeOff } from "react-icons/fi";
 
 export default function Registr() {
+  const [isVisible, setIsVisible] = useState(false);
+
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const detailsUser = Object.fromEntries(formData);
+    try {
+      // const res = await axiosReq({
+      //   method: "POST",
+      //   url: `/users/register`,
+      //   body: { ...detailsUser, fullName: fullName },
+      // });
+      console.log(detailsUser)
+      // if (res) navigate("/login");
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const formFields = [
+    { name: "firstName", placeholder: "* שם משתמש", type: "text", icon: <CiUser />, required: true },
+    { name: "email", placeholder: "* מייל", type: "email", icon: <HiOutlineMail />, required: true },
+    { name: "password", placeholder: "* סיסמא", type: isVisible ? "text" : "password", icon: <TbPassword />, required: true }
+  ];
+
   return (
-    <div className={styles.spinner}></div>
-  )
+    <form className={styles.container} onSubmit={handleSubmit}>
+      {formFields.map((field, index) => (
+        <div className={styles.imputContainer}>
+          <span className={styles.icon}>{field.icon}</span>
+          <input
+            key={index}
+            type={field.type}
+            className={styles.input}
+            placeholder={field.placeholder}
+            name={field.name}
+            required={field.required}
+          />
+          {field.name === "password" &&
+            <div onClick={() => setIsVisible(old => !old)}>
+              {isVisible ? <FiEyeOff /> : <FiEye />}
+            </div>
+          }
+        </div>
+      ))}
+      <input className={styles.btn} type="submit" value="הרשמה" />
+    </form>
+  );
 }
+
+
