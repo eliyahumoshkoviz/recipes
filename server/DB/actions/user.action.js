3dd80792072e3e407384ed101b89f92ff98c8389
meir vitkin <vitkin4580@email.com>
2024-06-24 18:45:36 +0300

    [user action file and utils folder]

---
+++ server/DB/actions/user.action.js
@@ -0,0 +1,14 @@
+import { createUserService } from "../service/user.service";
+
+
+export const createUserAction = async (fd) => {
+    const body = Object.fromEntries(fd);
+    console.log(body)
+
+    try {
+        const result = await createUserService(body);
+        console.log(result)
+    } catch (error) {
+        console.log({ error });
+    }
+};
