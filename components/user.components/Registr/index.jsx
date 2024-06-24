50ee89469a85eb21294fb14eecceb32d30967ef5
eliyahumoshkoviz <054epm@gmail.com>
2024-06-24 14:39:49 +0300

    add avartr at rgister

--- components/user.components/Registr/index.jsx
+++ components/user.components/Registr/index.jsx
@@ -6,6 +6,8 @@ import { CiUser } from "react-icons/ci";
 import { HiOutlineMail } from "react-icons/hi";
 import { TbPassword } from "react-icons/tb";
 import { FiEye, FiEyeOff } from "react-icons/fi";
+import { FaUserCircle } from "react-icons/fa";
+
 
 export default function Registr() {
   const [isVisible, setIsVisible] = useState(false);
@@ -29,9 +31,10 @@ export default function Registr() {
   };
 
   const formFields = [
-    { name: "firstName", placeholder: "* שם משתמש", type: "text", icon: <CiUser />, required: true },
+    { name: "userName", placeholder: "* שם משתמש", type: "text", icon: <CiUser />, required: true },
     { name: "email", placeholder: "* מייל", type: "email", icon: <HiOutlineMail />, required: true },
-    { name: "password", placeholder: "* סיסמא", type: isVisible ? "text" : "password", icon: <TbPassword />, required: true }
+    { name: "password", placeholder: "* סיסמא", type: isVisible ? "text" : "password", icon: <TbPassword />, required: true },
+    { name: "avatar", placeholder: "* תמונת פרופיל", type: "file", icon: <FaUserCircle />, required: false }
   ];
 
   return (
