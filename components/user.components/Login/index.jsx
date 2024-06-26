097901fc7938058100c01d9a0eae7b0cc252873d
eli <054apm@gmail.com>
2024-06-26 17:58:01 +0300

    add checkmark at login

--- components/user.components/Login/index.jsx
+++ components/user.components/Login/index.jsx
@@ -8,12 +8,19 @@ import { HiOutlineMail } from "react-icons/hi";
 import { TbPassword } from "react-icons/tb";
 import { FiEye, FiEyeOff } from "react-icons/fi";
 import { loginAction } from "@/server/DB/actions/user.action";
+import Checkmark from '@/components/Checkmark';
 
 export default function Login({ user, setIsLoggedIn }) {
   const [isVisible, setIsVisible] = useState(false);
   const [state, formAction] = useFormState(loginAction, undefined)
-  {state?.success !== undefined  && (setIsLoggedIn(state.success))}
 
+  const handleLoginSuccess = () => {
+    setTimeout(() => {
+      setIsLoggedIn(state.success)
+    }, 2000);
+  };
+
+  { state?.success !== undefined && (handleLoginSuccess()) }
 
   const formFields = [
     { name: "email", placeholder: "* מייל", type: "email", icon: <HiOutlineMail />, required: true, value: user?.email || '' },
@@ -21,27 +28,30 @@ export default function Login({ user, setIsLoggedIn }) {
   ];
 
   return (
-    <form className={styles.container} action={formAction}>
-      {formFields.map((field, index) => (
-        <div className={styles.imputContainer} key={index}>
-          <span className={styles.icon}>{field.icon}</span>
-          <input
-            defaultValue={field.value}
-            type={field.type}
-            className={styles.input}
-            placeholder={field.placeholder}
-            name={field.name}
-            required={field.required}
-          />
-          {field.name === "password" &&
-            <span onClick={() => setIsVisible(old => !old)}>
-              {isVisible ? <FiEyeOff /> : <FiEye />}
-            </span>
-          }
-        </div>
-      ))}
-      <input className={styles.btn} type="submit" value="התחברות" />
-    </form>
+    <>
+      <form className={styles.container} action={formAction}>
+        {formFields.map((field, index) => (
+          <div className={styles.imputContainer} key={index}>
+            <span className={styles.icon}>{field.icon}</span>
+            <input
+              defaultValue={field.value}
+              type={field.type}
+              className={styles.input}
+              placeholder={field.placeholder}
+              name={field.name}
+              required={field.required}
+            />
+            {field.name === "password" &&
+              <span onClick={() => setIsVisible(old => !old)}>
+                {isVisible ? <FiEyeOff /> : <FiEye />}
+              </span>
+            }
+          </div>
+        ))}
+        <input className={styles.btn} type="submit" value="התחברות" />
+      </form>
+      { state?.success !== undefined && <Checkmark/> }
+    </>
   );
 }
 
