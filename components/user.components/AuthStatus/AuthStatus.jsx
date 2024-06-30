779ce3b82e4fea4ed410bad180d465601e851d0b
eli <054apm@gmail.com>
2024-06-30 09:57:02 +0300

    add folder for checkTokn

--- components/user.components/AuthStatus/AuthStatus.jsx
+++ components/user.components/AuthStatus/AuthStatus.jsx
@@ -5,36 +5,19 @@ import styles from './style.module.scss'
 import Logged from '../Logged';
 import Guest from '../guest';
 import { deleteCookie, getCookies, hasCookie } from 'cookies-next';
+import CheckToken from '@/components/CheckToken';
 
 
 const AuthStatus = () => {
     const [isLoggedIn, setIsLoggedIn] = useState(null);
 
-    const getAuthStatus = async () => {
-        if (!hasCookie('name')) return
-        if (!hasCookie('avatar')) return
-        let { name, avatar } = getCookies();
-        name = decodeURIComponent(name);
-        avatar = decodeURIComponent(avatar);
-        try {
-            const response = await fetch('/api/user', { method: "POST" })
-            const { _id } = await response?.json();
-            _id && setIsLoggedIn({ userlogged: { name, avatar } });
-        } catch (error) {
-            deleteCookie('name')
-            deleteCookie('avatar')
-            console.error("Fetch error:", error);
-        }
-
-    };
-    useEffect(() => {
-        getAuthStatus();
-    }, []);
-
     return (
+        <>
+        <CheckToken setIsLoggedIn={setIsLoggedIn}/>
         <div className={styles.login}>
             {isLoggedIn ? <Logged isLoggedIn={isLoggedIn} /> : <Guest setIsLoggedIn={setIsLoggedIn} />}
         </div>
+        </>
 
     );
 }
