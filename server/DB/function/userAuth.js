a0bafa11181e3be167303d7038225eabf62fb536
eliyahumoshkoviz <054epm@gmail>
2024-07-01 23:51:47 +0300

    fix userAuth

--- server/DB/function/userAuth.js
+++ server/DB/function/userAuth.js
@@ -2,24 +2,32 @@ import { cookies } from "next/headers";
 import { checkToken } from "@/server/DB/utils/jwt";
 
 export const getPermission = () => {
-    const cookieStore = cookies()
-    const token = cookieStore.get("token")?.value
-    if (!token) return null
-    const { permission } = checkToken(token)
-    return permission
-}
+  const cookieStore = cookies();
+  const token = cookieStore.get("token")?.value;
+  if (!token) return null;
+  const { permission } = checkToken(token);
+  return permission;
+};
 
 export const isAdmin = () => {
-    const cookieStore = cookies()
-    const token = cookieStore.get("token")?.value
-    if (!token) return false //throw new Error ({message: 'token not found'})
-    const { permission } = checkToken(token)
-    return permission === 'admin'
-}
+  const cookieStore = cookies();
+  const token = cookieStore.get("token")?.value;
+  if (!token) return null; //throw new Error ({message: 'token not found'})
+  try {
+    const { permission } = checkToken(token);
+    return permission === "admin";
+  } catch (e) {
+    return null; //throw new Error ({message: 'token not found'})
+  }
+};
 export const isEditor = () => {
-    const cookieStore = cookies()
-    const token = cookieStore.get("token")?.value
-    if (!token) return false //throw new Error ({message: 'token not found'})
-    const { permission } = checkToken(token)
-    return permission === 'editor' || permission === 'admin'
-}
+  const cookieStore = cookies();
+  const token = cookieStore.get("token")?.value;
+  if (!token) return null; //throw new Error ({message: 'token not found'})
+  try {
+    const { permission } = checkToken(token);
+    return permission === "editor" || permission === "admin";
+  } catch (e) {
+    return null; //throw new Error ({message: 'token not found'})
+  }
+};
