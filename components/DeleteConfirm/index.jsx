3eb45474980524a42c6d03b262b57b48e86bcb98
eli <054apm@gmail.com>
2024-07-04 17:00:59 +0300

    add animation for del category

--- components/DeleteConfirm/index.jsx
+++ components/DeleteConfirm/index.jsx
@@ -1,20 +1,25 @@
 import React, { useContext } from "react";
 import styles from "./style.module.scss";
 import { useRouter } from "next/navigation";
+import SuccessMessage from "../SuccessMessage";
 
 export const DeleteConfirm = ({ type, setPopup, _id, category, title }) => {
   const router = useRouter();
 
   const handleDelete = async () => {
       const options = { method: "DELETE", cache: "no-store" };
-      if (category) {
-        options.body = JSON.stringify({ category });
-      }
+
+      if (category) options.body = JSON.stringify({ category });
+
       const response = await fetch(`/api/${type}/${_id}`, options);
       const data = await response?.json();
+
       if (data.message) {
         router.push(`/${category ? `category/${title}` : ''}`, undefined, { shallow: false });
-        setPopup(undefined);
+        setPopup(<SuccessMessage message={`${data.message}`}/>);
+        setTimeout(() => {
+          setPopup(undefined);
+        }, 2000);
       }
       else{
         setPopup(`${data.error}`);
